972c08e939fc0ad92ee093358809c6ac2cf33752
Zane Godden <zane@jackdaw.dev>
2025-12-07 18:57:18 +0000

    consolidate more configs into sway

--- copy_over_system.sh
+++ copy_over_system.sh
@@ -1,3 +1,4 @@
 cp -R ./.config/* ~/.config/
 cp -R ./.themes/* ~/.themes/
 cp -R ./.local/* ~/.local/
+cp -R ./.icons/* ~/.icons/
--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -3,21 +3,11 @@
 # Script to copy dotfiles to git directories
 #
 
-rsync -a --mkpath --delete /home/zane/.config/kitty/* .config/kitty/
-
-rsync -a --mkpath --delete /home/zane/.config/mako/* .config/mako/
-
 rsync -a --mkpath --delete /home/zane/.config/sway/* .config/sway/
 
 rsync -a --mkpath --delete /home/zane/.config/rofi/* .config/rofi/
 
 rsync -a --mkpath --delete /home/zane/.config/tofi/* .config/tofi/
-
-rsync -a --mkpath --delete /home/zane/.config/waybar/* .config/waybar/
-
-rsync -a --mkpath --delete /home/zane/.config/swaylock/* .config/swaylock/
-
-rsync -a --mkpath --delete /home/zane/.config/swayidle/* .config/swayidle/
 
 rsync -a --mkpath --delete /home/zane/.config/qt6ct/* .config/qt6ct/
 
